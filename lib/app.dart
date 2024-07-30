d56555d4d341a30ff1b967f4e9b711f88f818536
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-07-31 03:33:15 +0600

    app.dart, language_translation_screen.dart, and translator dependency are added

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lets Translate',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
---
+++ lib/features/language_translation_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageTranslationScreen extends StatefulWidget {
+  const LanguageTranslationScreen({super.key});
+
+  @override
+  State<LanguageTranslationScreen> createState() => _LanguageTranslationScreenState();
+}
+
+class _LanguageTranslationScreenState extends State<LanguageTranslationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
